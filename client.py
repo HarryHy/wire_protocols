3e55cdfadfe567b6114699a569f961eedaf861bf
Xinyu Kang <kangxinyu0423@126.com>
2023-02-06 22:36:59 -0500

    fix signup, delete all jsons on the client side

--- client.py
+++ client.py
@@ -11,42 +11,50 @@ username = "example"
 password = "123"
 
 
+# def check_dupname():
+
+
+
 
 def login():
+    client.send('LOGIN'.encode('ascii'))
     os.system("cls||clear")
     global username
     global password
     username = input("Enter the username: ")
     password = input("Enter the password: ")
-    # check the validy of the login at the server side
-    global client
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect to a host
-    client.connect((host, port))
+    
+    
 
 
-def signup():
+def signup():  
     os.system('cls||clear')
+    # check if username is unique
     global username
     global password
     while True:
-        username = input("Create your username: ")
-        # check if username is unique
-        with open("accounts.json", "r") as f:
-            data = json.load(f)
-        if username in data:
-            print("Username already exists! Change to another one.")
-        else:
-            break
-    password = input("Create your password: ")
-    # store the new created account into the json file
-    with open("accounts.json", "w") as f:
-        data[username] = {"password": password}  # TODO: check both integers and strings work
-        json.dump(data, f, indent=4)
+        try:
+            username = input("Create your username: ")
+            # send the username to server for duplicate check
+            client.send(('SIGNUP '+username).encode('ascii'))
+            dup_message = client.recv(1024).decode('ascii')
+            if dup_message == "DUPNAME":
+                print("Username already exists! Change to another one.")
+            elif dup_message == "NONDUPNAME":
+                password = input("Create your password: ")
+                client.send(password.encode('ascii'))
+                break
+
+        except Exception as e:
+            print('Error Occurred: ', e)
+            client.close()
+        
+
     
 def listAccounts():
     # list all or a subset of the accounts by text wildcard
     # TODO
+    os.system('cls||clear')
     return 
 
 
@@ -62,20 +70,23 @@ def receive():
                 if next_message == 'PASSWORD':
                     client.send(password.encode('ascii'))
                     if client.recv(1024).decode('ascii') == "REJECT":
-                        print("Wrong password!")
+                        print("Wrong password! Try again")
                         stop = True
-                    # else:
+                    # elif :
                     #     # TODO
+                # elif next_message == 'DUPNAME':
+            elif message == "FAIL":
+                print("You've reach the attemp limit, connection failed.")
             else: 
                 print(message)
         except Exception as e:
             print('Error Occurred: ', e)
             client.close()
             break
+    choose_operations()
 
-def main():
+def choose_operations():
     while True:
-        os.system("cls||clear")
         option = input("(1)Sign in\n(2)Sign up\n(3)List existing accounts\n")
         if option == "1":
             login()
@@ -84,9 +95,19 @@ def main():
             signup()
         elif option == "3":
             listAccounts()
-
     recieve_thread = threading.Thread(target=receive)
     recieve_thread.start()
+
+
+def main():
+    global client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to the host
+    client.connect((host, port))
+    os.system("cls||clear")
+    choose_operations()
+
+
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -6,6 +6,9 @@ host = "100.91.14.32"
 port = 5555
 # keep a set of already logged in users
 logins = set()
+# keep a number of logged in times, exceeding this limit will cause break of the connection
+LOGIN_LIMIT = 5
+login_times = 0
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -13,25 +16,50 @@ server.listen()
 print('Listening...')
 
 def receive():
+    # connect with the client
+    client, addr = server.accept()
+    print(f"Connected with {str(addr)}")
     while True:
-        # connect with the client
-        client, addr = server.accept()
-        print(f"Connected with {str(addr)}")
-        # ask the client for the username
-        client.send("USERNAME".encode('ascii'))
-        username = client.recv(1024).decode('ascii')
-        client.send('PASSWORD'.encode('ascii'))
-        password = client.recv(1024).decode('ascii')
-        # check if password matches with user name
-        with open('accounts.json') as f:
-            data = json.load(f)
-        if password != data[username]["password"]:
-            client.send("REJECT".encode('ascii'))
-            client.close()
-            continue # returns the control to the beginning of the while loop
-        else:
-            print("Successfully logged in!")
-            logins.add(username)
+        
+        operation = client.recv(1024).decode('ascii')
+        if operation == "LOGIN":
+            login_times += 1
+            if login_times > LOGIN_LIMIT:
+                client.send("FAIL".encode('ascii'))
+                client.close()
+
+            # check whether username and password match
+            # ask the client for the username
+            client.send("USERNAME".encode('ascii'))
+            username = client.recv(1024).decode('ascii')
+            client.send('PASSWORD'.encode('ascii'))
+            password = client.recv(1024).decode('ascii')
+            # check if password matches with user name
+            with open('accounts.json') as f:
+                data = json.load(f)
+            if password != data[username]["password"]:
+                print("Login rejected!")
+                client.send("REJECT".encode('ascii'))
+                continue # returns the control to the beginning of the while loop
+            else:
+                print("Successfully logged in!")
+                logins.add(username)
+        elif operation.startswith("SIGNUP"):
+            # check whether the username already exists
+            username = operation.split(" ")[1]
+            with open('accounts.json') as f:
+                data = json.load(f)
+            if username in data:
+                client.send('DUPNAME'.encode('ascii'))
+            else:
+                client.send('NONDUPNAME'.encode('ascii'))
+                password = client.recv(1024).decode('ascii')
+                # store the new created account into the json file
+                with open("accounts.json", "w") as f:
+                    data[username] = {"password": password}
+                    json.dump(data, f, indent=4)
+            
+        
 
 
 
