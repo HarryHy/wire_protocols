b9152715ba0c9c9ebab310aa80304fbe05e578ee
Harry <30531325+HarryHy@users.noreply.github.com>
2023-02-10 21:04:01 -0500

    change the global stop
    To support the login part to repeat

--- client.py
+++ client.py
@@ -1,141 +1,162 @@
 import os
 import threading
 import socket
-import json
 import argparse
+import pickle
+
 # global variables
 stop = False
+username = "example"
+password = "123"
 
+
+# def check_dupname():
 
 
 def login():
+    client.send('LOGIN'.encode('ascii'))
     os.system("cls||clear")
-    # load all accounts
-
- 
     global username
     global password
-    username = input("Enter the username:")
+    username = input("Enter the username: ")
     password = input("Enter the password: ")
-    # check the validy of the login at the server side
-    global client
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect to a host
-    client.connect((host, port))
-    client.send('login'.encode('ascii'))
+    global stop
+    stop = False
+    
+    
 
+
+def signup():  
+    os.system('cls||clear')
+    # check if username is unique
+    global username
+    global password
     while True:
-        global stop
-        if stop: break
         try:
+            username = input("Create your username: ")
+            # send the username to server for duplicate check
+            client.send(('SIGNUP '+username).encode('ascii'))
+            dup_message = client.recv(1024).decode('ascii')
+            if dup_message == "DUPNAME":
+                print("Username already exists! Change to another one.")
+            elif dup_message == "NONDUPNAME":
+                password = input("Create your password: ")
+                client.send(password.encode('ascii'))
+                break
+
+        except Exception as e:
+            print('Error Occurred: ', e)
+            client.close()
+        
+
+    
+def listAccounts():
+    os.system('cls||clear')
+    # list all or a subset of the accounts by text wildcard
+    while True:
+        option = input("(1)List all \n(2)List by wildcard\n")
+        if option == "1":
+            client.send('LIST ALL'.encode('ascii'))
+            break
+        elif option == "2":
+            pattern = input("Input your search pattern: ")
+            client.send(('LIST '+pattern).encode('ascii'))
+            break
+        else: 
+            print("Invalid option, choose again")
+    
+    response = client.recv(1024).decode('ascii')
+    if response == "NOMATCHED":
+        print("No matched account found")
+    elif response == "MATCHED":
+        client.send("SENDMATCHED".encode('ascii'))
+        list_bytes = client.recv(4096)
+        list_accounts = pickle.loads(list_bytes)
+        for a in list_accounts:
+            print(a)
+        
+
+def receive():
+    print("in receive")
+    try:
+        while True:
+            global stop
+            if stop: break
+        
             message = client.recv(1024).decode('ascii')
+            print("message is ", message)
             if message == "USERNAME":
+                print("client trying to send username")
                 client.send(username.encode('ascii'))
                 next_message = client.recv(1024).decode('ascii')
                 if next_message == 'PASSWORD':
                     client.send(password.encode('ascii'))
-                    if client.recv(1024).decode('ascii') == "REJECT":
-                        print("Wrong password!")
+                    check_state = client.recv(1024).decode('ascii')
+                    if check_state == "REJECT":
+                        print("Wrong password! Try again")
                         stop = True
-                    # else:
-                    #     # TODO
+                    elif check_state == "NOUSER":
+                        print("No such user")
+                        stop = True
+                    else:
+                        #print(s)
+                        print("Successfully logged in as ", username)
+                        retry = input("Retry ? y/n")
+                        if retry is "y":
+                            client.send('RESTART'.encode('ascii'))
+                            choose_operations()
+                        else:
+                            #TODO 
+                            client.send('BREAK'.encode('ascii'))
+                            return
+                # elif next_message == 'DUPNAME':
+            elif message == "FAIL":
+                print("You've reach the attemp limit, connection failed.")
             else: 
+                print(" the message is not on the list")
                 print(message)
-        except Exception as e:
-            print('Error Occurred: ', e)
-            client.close()
-            break
-
-        print("login successfully")
-        return
-
-def signup():
-    os.system("cls||clear")
-    # load all accounts
-
-    global username
-    global password
-    global client
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect to a host
-    client.connect((host, port))
-
-    try:
-        client.send('signup'.encode('ascii'))
-
-        username = input("Create a username:")
-        client.send(username.encode('ascii'))
-        message = client.recv(1024).decode('ascii')
-        print(message)
-        if message == "USERNAME":
-            next_message = client.recv(1024).decode('ascii')
-            
-            if next_message == 'PASSWORD':
-                print("2")
-                password = input("Create the password: ")
-                client.send(password.encode('ascii'))
-                print("sign up successfully")
-            elif next_message == "INVALID": 
-                print("Invalid username")
-                retry = input("Retry ? y/n")
-                if retry is "y":
-                    signup()
-                else:
-                    return
-
-
+        print("out of while loop")
     except Exception as e:
         print('Error Occurred: ', e)
         client.close()
-        return
 
+    #client.send('RESTART'.encode('ascii'))
+    choose_operations()
 
-
-def receive():
+def choose_operations():
     while True:
-        global stop
-        if stop: break
-        try:
-            message = client.recv(1024).decode('ascii')
-            if message == "USERNAME":
-                client.send(username.encode('ascii'))
-                next_message = client.recv(1024).decode('ascii')
-                if next_message == 'PASSWORD':
-                    client.send(password.encode('ascii'))
-                    if client.recv(1024).decode('ascii') == "REJECT":
-                        print("Wrong password!")
-                        stop = True
-                    # else:
-                    #     # TODO
-            else: 
-                print(message)
-        except Exception as e:
-            print('Error Occurred: ', e)
-            client.close()
-            break
-
-def main():
-    while True:
-        os.system("cls||clear")
-        option = input("(1)Sign in\n(2)Sign up \n")
+        option = input("(1)Sign in\n(2)Sign up\n(3)List existing accounts\n")
         if option == "1":
             login()
             break
         elif option == "2":
             signup()
-            break
-    print("start to chat")
-    #recieve_thread = threading.Thread(target=receive)
-    #recieve_thread.start()
+        elif option == "3":
+            listAccounts()
+        else: 
+            print("Invalid option, choose again")
+    recieve_thread = threading.Thread(target=receive)
+    recieve_thread.start()
+
+
+def main():
+    global client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to the host
+    client.connect((host, port))
+    os.system("cls||clear")
+    choose_operations()
+
+
 
 
 if __name__ == '__main__':
     parse = argparse.ArgumentParser()
-    parse.add_argument('-u', dest = 'host' , help = '172.16.225.218')
+    parse.add_argument('-u', dest = 'host' , help = '127.0.0.1')
     parse.add_argument('-p', dest = 'port' ,type=int,help = '9999')
     args = parse.parse_args()
-    global host 
-    global port 
+    global host
+    global port
     host = args.host
     port = args.port
     main()
--- server.py
+++ server.py
@@ -1,82 +1,106 @@
 import threading
 import socket
 import json
+import pickle
+import fnmatch
 
-host = "10.250.183.6"
-port = 5555
-# keep a set of already logged in users
-logins = set()
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
-print('Listening...')
 
-def receive():
-    try:
-        with open('accounts.json') as f:
-            data = json.load(f)
-        while True:
-            # connect with the client
-            client, addr = server.accept()
-            print(f"Connected with {str(addr)}")
-            # ask the client for the username
-            #client.send("USERNAME".encode('ascii'))
-            opt = client.recv(1024).decode('ascii')
-
-            if opt == 'login':
-                print("logging in ")
-                client.send("USERNAME".encode('ascii'))
-                username = client.recv(1024).decode('ascii')
-                
-                if client not in logins: # client is not log in before. 
-                    client.send('PASSWORD'.encode('ascii'))
-                    password = client.recv(1024).decode('ascii')
-
-                    # check if not password matches with user name
-                    if str(password) != str(data[username]["password"]):
-                        client.send("REJECT".encode('ascii'))
-                        client.close()
-                        continue # returns the control to the beginning of the while loop
-                    else:
-                        print("Successfully logged in!")
-                        client.send("Successfully logged in!".encode('ascii'))
-                        logins.add(client)
-                        client.close()
-                        continue
-            
-            elif opt == 'signup':
-                print("signup ")
-                with open('accounts.json') as f:
-                    data = json.load(f)
-
-                client.send("USERNAME".encode('ascii'))
-                username = client.recv(1024).decode('ascii')
-                print('username is', username)
-                print( list(data.keys()))
-                if str(username) not in list(data.keys()): # client is not log in before. 
-                    print("1")
-                    client.send('PASSWORD'.encode('ascii'))
-                    user_password = client.recv(1024).decode('ascii')
-                    with open('accounts.json', 'r') as f:
-                        data = json.load(f)
-                    # Store the info of the new server in servers.json
-                    with open('accounts.json', 'w') as f:
-                        data[username] = {"password":user_password}
-                        json.dump(data, f, indent=4)
-                    logins.add(client)
-                    client.close()
-                    continue
-                else:
-                    client.send('INVALID'.encode('ascii'))
-                    client.close()
-                    continue
-    except Exception as e:
-            print('Error Occurred: ', e)
-            server.close() 
-                
+def receive(LOGIN_LIMIT = 5, login_times = 0):
+    # connect with the client
+    client, addr = server.accept()
+    print(f"Connected with {str(addr)}")
+    while True:
         
+        operation = client.recv(1024).decode('ascii')
+        print("operation is ", operation)
+        if operation == "LOGIN":
+            login_times += 1
+            print("logging")
+            if login_times > LOGIN_LIMIT:
+                client.send("FAIL".encode('ascii'))
+                client.close()
 
+            # check whether username and password match
+            # ask the client for the username
+            client.send("USERNAME".encode('ascii'))
+            print("already sent USERNAME")
+            username = client.recv(1024).decode('ascii')
+            print("username is ", username)
+            client.send('PASSWORD'.encode('ascii'))
+            password = client.recv(1024).decode('ascii')
+            print("password is ", password)
+            # check if password matches with user name
+            with open('accounts.json') as f:
+                data = json.load(f)
+            if username not in data.keys():
+                print("No such user")
+                client.send("NOUSER".encode('ascii'))
+            elif password != data[username]["password"]:
+                print("Login rejected!")
+                client.send("REJECT".encode('ascii'))
+                #continue # returns the control to the beginning of the while loop
+            else:
+                print("Successfully logged in! as ", username)
+                client.send("ACCEPT".encode('ascii'))
+                logins.add(username)
+                #continue
+            #TODO
+        elif operation.startswith("SIGNUP"):
+            # check whether the username already exists
+            username = operation.split(" ")[1]
+            with open('accounts.json') as f:
+                data = json.load(f)
+            if username in data:
+                client.send('DUPNAME'.encode('ascii'))
+            else:
+                client.send('NONDUPNAME'.encode('ascii'))
+                password = client.recv(1024).decode('ascii')
+                # store the new created account into the json file
+                with open("accounts.json", "w") as f:
+                    data[username] = {"password": password}
+                    json.dump(data, f, indent=4)
+        elif operation.startswith("LIST"):
+            pattern = operation.split(" ")[1]
+            with open('accounts.json') as f:
+                    data = json.load(f)
+            if pattern == "ALL":
+                keys = list(data.keys())
+            else:
+                keys = []
+                for k in list(data.keys()):
+                    if fnmatch.fnmatch(k, pattern):
+                        keys.append(k)
+            if len(keys) == 0:
+                client.send("NOMATCHED".encode('ascii'))
+            else:
+                client.send("MATCHED".encode('ascii'))
+                next_line = client.recv(1024).decode('ascii')
+                if next_line == "SENDMATCHED":
+                    lists = pickle.dumps(keys)
+                    client.send(lists)   
+        
+        elif operation.startswith("BREAK"):
+            break
+        
+                    
 
+if __name__ == '__main__':
+    host = "127.0.0.1"
+    port = 5555
+    # keep a set of already logged in users
+    logins = set()
+    # keep a number of logged in times, exceeding this limit will cause break of the connection
+    LOGIN_LIMIT = 5
 
-receive()
+    login_times = 0
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    print('Listening...')
+    try:
+        receive(LOGIN_LIMIT, login_times )
+    except Exception as e:
+        print('Error Occurred: ', e)
+        server.close() 
